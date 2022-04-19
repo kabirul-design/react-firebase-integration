dd0a93189cea457e334c0bdd2d8687655771d437
kabirulpHero <kabirulislamuo@gmail.com>
2022-04-20 03:22:07 +0600

    firebase state management

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,16 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
 
+
+const auth = getAuth(app)
 const Home = () => {
+    const [user] = useAuthState(auth);
     return (
         <div>
             <h2>This is Home</h2>
+            <p>UserName : {user?.displayName ? user.displayName : 'No Body'}</p>
         </div>
     );
 };
