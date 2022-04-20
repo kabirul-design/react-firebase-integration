f971c915f9cc5398d32530c1a66a622d5b48a272
kabirulpHero <kabirulislamuo@gmail.com>
2022-04-21 01:34:26 +0600

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import Register from './components/Register/Register';
+import Products from './components/Products/Products';
+import Order from './components/Order/Order';
+import RequireAuth from './components/RequirAuth/RequirAuth';
 
 function App() {
   return (
@@ -12,8 +15,14 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/products' element={
+          <RequireAuth><Products></Products></RequireAuth>
+        }></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/order' element={
+          <RequireAuth><Order></Order></RequireAuth>
+        }></Route>
       </Routes>
     </div>
   );
